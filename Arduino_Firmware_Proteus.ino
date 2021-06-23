c6428ebc1a6b7c803cf07b7a0698a091f5df6e23
Bhuvan Pratap Agarwal <60248682+Bhuvan-Pratap-Agarwal@users.noreply.github.com>
2021-06-23 15:18:25 +0530

    Update Arduino_Firmware_Proteus.ino

--- Arduino_Firmware_Proteus.ino
+++ Arduino_Firmware_Proteus.ino
@@ -1,9 +1,9 @@
 //===================================================================================  Declaring   ==============================================================================
 
-#include <servo.h>
+#include <Servo.h>
 #include <Wire.h>
-#include <Time.h>
-#include <DS1307RTC.h>
+#include <time.h>
+//#include <ds1307rtc.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <SoftwareSerial.h>
@@ -22,35 +22,35 @@ int pos = 0;
 
 
 //ir
-int IRH = 10;
-int IRO = 11;
+int IRH = 2;
+int IRO = 3;
 
 //RTC
-RTC_DS1307 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//RTC_DS1307 rtc;
+//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 //Buzzer
-int buz 6;
+int buz = 24;
 
 //line follower
-#define in1 25
-#define in2 24
-#define in3 23
-#define in4 22
-#define enA 30
-#define enB 31
-int M1_Speed = 100; // speed of motor 
-int M2_Speed = 100; // speed of motor 2
-int LeftRotationSpeed = 250;  // Left Rotation Speed
-int RightRotationSpeed = 250; // Right Rotation Speed
+#define in1 30
+#define in2 31
+#define in3 32
+#define in4 33
+#define enA A2
+#define enB A3
+int M1_Speed = 250; // speed of motor 
+int M2_Speed = 250; // speed of motor 2
+int LeftRotationSpeed = 500;  // Left Rotation Speed
+int RightRotationSpeed = 500; // Right Rotation Speed
 
 //LED
-int Rled 8;
-int Gled 7;
+int Rled = 23;
+int Gled = 22;
 
 //miscellaneous
 
-int rand;
+int num;
 
 
 
@@ -59,6 +59,7 @@ int rand;
 
 void setup() 
 {
+  Serial.begin(9600);
   //Line FOllower  
   pinMode(in1,OUTPUT);
   pinMode(in2,OUTPUT);
@@ -71,24 +72,24 @@ void setup()
 
    //RTC
   //  while (!Serial); // for Leonardo/Micro/Zero
-  Serial.begin(9600);
-  if (! rtc.begin())
-  {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-
-  if (! rtc.isrunning())
-  {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-//     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    rtc.adjust(DateTime(2021, 6, 22, 10, 24, 15));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  } 
-
+//  Serial.begin(9600);
+//  if (! rtc.begin())
+//  {
+//    Serial.printlnln("Couldn't find RTC");
+//    while (1);
+//  }
+//
+//  if (! rtc.isrunning())
+//  {
+//    Serial.printlnln("RTC is NOT running!");
+//    // following line sets the RTC to the date & time this sketch was compiled
+////     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+//    rtc.adjust(DateTime(2021, 6, 22, 10, 24, 15));
+//    // This line sets the RTC with an explicit date & time, for example to set
+//    // January 21, 2014 at 3am you would call:
+//    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+//  } 
+//
 
 
   //IR Sensor
@@ -96,20 +97,20 @@ void setup()
   pinMode (IRO, INPUT); // sensor pin INPUT for object detection 
 
   //Servo Motors
-  myservo.attach(2);
-  myservo.attach(3);
-  myservo.attach(4);
-  myservo.attach(12);
+  myservo1.attach(25);
+  myservo2.attach(26);
+  myservo3.attach(27);
+  myservo4.attach(28);
   
   
   //Buzzer
-  digitalWrite(buz, OUTPUT);
+  pinMode(buz, OUTPUT);
 
 
   //LED
-  digitalWrite(Rled, OUTPUT);
-  digitalWrite(Gled, OUTPUT);
-
+  pinMode(Rled, OUTPUT);
+  pinMode(Gled, OUTPUT);
+}
 
 //===================================================================================  Void Loop  ===============================================================================
 
@@ -127,10 +128,51 @@ void loop()
 
 
 
+//void linefollower() //line follower
+//{
+//  Serial.println("Starting to move");
+//  int LEFT_SENSOR = digitalRead(A0);
+//  int RIGHT_SENSOR = digitalRead(A1);
+////  if(RIGHT_SENSOR==0 && LEFT_SENSOR==0) 
+////  {
+////    forward(); //FORWARD
+////  }
+////  else if(RIGHT_SENSOR==0 && LEFT_SENSOR==1) 
+////  {
+////    right(); //Move Right
+////  }
+////  else if(RIGHT_SENSOR==1 && LEFT_SENSOR==0) 
+////  {
+////    left(); //Move Left
+////  }
+////  else if(RIGHT_SENSOR==1 && LEFT_SENSOR==1) 
+////  {
+//
+//   forward();
+//   delay(10000);
+//   right();
+//   delay(5000);
+//   forward();
+//   delay(3000);
+//   left();
+//   delay(2000);
+//   Stop();  //STOP
+//   right();
+//   delay(5000);
+//   Stop();
+//   despensing(); // despensing or RIFD have to read RFID and choose
+//   delay(10000);
+//   left();
+//   delay(5000);
+//   Stop();
+//   forward();
+////  }
+//}  
+
 void linefollower() //line follower
 {
-  int LEFT_SENSOR = digitalRead(A0);
-  int RIGHT_SENSOR = digitalRead(A1);
+  int LEFT_SENSOR = digitalRead(A1);
+  int RIGHT_SENSOR = digitalRead(A0);
   if(RIGHT_SENSOR==0 && LEFT_SENSOR==0) 
   {
     forward(); //FORWARD
@@ -147,19 +189,20 @@ void linefollower() //line follower
   {
    Stop();  //STOP
    right();
-   delay(5000);
-   stop();
+   delay(500);
+   Stop();
    despensing(); // despensing or RIFD have to read RFID and choose
-   delay(10000);
+   delay(500);
    left();
-   delay(5000);
-   stop();
+   delay(500);
+   Stop();
    forward();
   }
-}  
+} 
  
 void forward()
 {
+  Serial.println("Line follower Moving Forward");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
@@ -169,6 +212,7 @@ void forward()
 }
 void backward()
 {
+  Serial.println("Line follower Moving Backwards");
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
@@ -178,6 +222,7 @@ void backward()
 }
 void right()
 {
+  Serial.println("Line follower Turning Right");
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
@@ -187,6 +232,7 @@ void right()
 }
 void left()
 {
+  Serial.println("Line follower Turning Left");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -196,6 +242,7 @@ void left()
 }
 void Stop()
 {
+  Serial.println("Line follower Has Stopped Moving");
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -209,89 +256,96 @@ void Stop()
 
 void despensing() //Despensing system
 {
-  rand = random(3);
-  switch(rand)
+  num = random(3);
+  switch(num)
   {
     case 0: 
+      Serial.println("Despensing patient 1 Medicines");
       motor1();
       motor2();
       motor3();
       break;
     
     case 1:
+      Serial.println("Despensing patient 2 Medicines");
       motor2();
       motor1();
       motor3();
       break;
     
     case 2:
+      Serial.println("Despensing patient 3 Medicines");
       motor3();
       motor2();
       motor1();
       break;
   }
   
-  delay(2000);
+  delay(500);
   irsensordespensor();
 }
 
 void motor1()
 {
+  Serial.println("Motor 1 turning");
   for (pos = 0; pos <= 180; pos += 1)
   { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
     delay(3);                       // waits 15 ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) 
   { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
     delay(3);                       // waits 15 ms for the servo to reach the position
   }
 }
 
 void motor2()
 {
+  Serial.println("Motor 2 turning");
     for (pos = 0; pos <= 180; pos += 1)
   { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    myservo2.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15 ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) 
   { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    myservo2.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15 ms for the servo to reach the position
   }
 }
 
 void motor3()
 {
+  Serial.println("Motor 3 turning");
     for (pos = 0; pos <= 180; pos += 1)
   { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    myservo3.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15 ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) 
   { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    myservo3.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15 ms for the servo to reach the position
   }
 }
 
 void motor4()//main dispeinsing
 {
+  Serial.println("Motor 4 turning");
     for (pos = 0; pos <= 180; pos += 1)
   { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    myservo4.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15 ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) 
   { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    myservo4.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15 ms for the servo to reach the position
   }
 }
 
@@ -299,35 +353,37 @@ void motor4()//main dispeinsing
 
 //=====================================================================================  RTC  ===================================================================================
 
-void time()
-{
-  DateTime now = rtc.now();
-  int date_time[5]={now.day(), now.month(), now.year(), now.hour(), now.minute()};
-  Serial.print(date_time);
-  delay(3000);
-}
+//void time()
+//{
+//  DateTime now = rtc.now();
+//  int date_time[5]={now.day(), now.month(), now.year(), now.hour(), now.minute()};
+//  Serial.println(date_time);
+//  delay(500);
+//}
 
 
 //===================================================================================  Buzzers  =================================================================================
 
 void buzzReady()
 {
+  Serial.println("Buzzing for medicine reminder");
   digitalWrite(buz, HIGH);
-  delay(2000);
+  delay(500);
   digitalWrite(buz, LOW);
-  delay(2000);
+  delay(500);
   digitalWrite(buz, HIGH);
-  delay(2000);
+  delay(500);
   digitalWrite(buz, LOW);  
 }
 
 
 void buzzDone()
 {
+  Serial.println("Buzzing for moving on");
   digitalWrite(buz, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(buz, LOW);
-  delay(1000);
+  delay(500);
   digitalWrite(buz, HIGH);
   delay(1000);
   digitalWrite(buz, LOW);
@@ -337,12 +393,14 @@ void buzzDone()
 
 void red_led()
 {
+  Serial.println("Red Led Glowing");
   digitalWrite(Rled, HIGH);
 }
 
 
 void green_led()
 {
+  Serial.println("Green Led Glwoing");
   digitalWrite(Gled, HIGH);
 }
 
@@ -351,8 +409,22 @@ void green_led()
 
 void irsensordespensor() //despensing gate way
 {
-  int statusSensor = digitalRead(IRSensor); // reads the IR sensor input
-  
+  Serial.println("Medicines Dispensing");
+//  Serial.println("");
+  buzzReady();
+  red_led();
+  delay(500);
+  int statusSensor = digitalRead(IRH); // reads the IR sensor input  
   if (statusSensor == 0)
      motor4();
+  else
+    delay(3000);
+    Serial.println("The pills are ready please collect them");
+  buzzDone();
+  digitalWrite(Rled, LOW);
+  Serial.println("Red Led Stopped Glowing");
+  green_led();
+  delay(500);
+  digitalWrite(Gled, LOW);
+  Serial.println("Green Led Stopped Glowing");
 }
